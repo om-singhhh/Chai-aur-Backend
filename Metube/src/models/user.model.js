c80f8b969785503c7bbbce45a8d94df04b57f898
OmSingh04 <omsingh18609@gmail.com>
2026-02-09 12:23:45 +0530

    Resolving Password issues  in userModel

--- Metube/src/models/user.model.js
+++ Metube/src/models/user.model.js
@@ -1,4 +1,7 @@
 import mongoose from "mongoose"
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
 
 const userSchema = new mongoose.Schema(
     {
@@ -53,4 +56,20 @@ const userSchema = new mongoose.Schema(
     {timestamps:true}
 )
 
+
+userSchema.pre("save", async function (next) {
+    // there is problem that how many data saved , this function saves or encrypt the passoword
+    if(!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password,10)
+    next()
+})
+
+//csutom models
+userSchema.methods.isPasswordCorrect = async function (password)
+{
+   return await bcrypt.compare(password,this.password)
+}
+
 export const User =  new mongoose.models("User",userSchema)
+
+// direct encryption is not possible so we use bcryptjs to hash the password and for token management we use jwt token and we have to use mongoose hooks
