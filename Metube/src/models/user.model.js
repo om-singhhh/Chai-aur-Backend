c426606cb71f2bfb0551184d1dbf7070c720b2f0
OmSingh04 <omsingh18609@gmail.com>
2026-02-09 11:46:07 +0530

    Updating user model

--- Metube/src/models/user.model.js
+++ Metube/src/models/user.model.js
@@ -2,23 +2,52 @@ import mongoose from "mongoose"
 
 const userSchema = new mongoose.Schema(
     {
-        name : {
+        username : {
             type : String,
             required :true,
+            unique : true,
+            lowercase : true,
+            trim:true,
+            index:true,
 
         },
         email : {
             type : String,
             required : true,
+             unique : true,
+            lowercase : true,
+            trim:true,
+            index:true,
 
         },
-        username :{
+        fullName :{
             type :String,
-            required : true
+            required : true,
+            trim:true,
+            index:true,
         },
         password :{
             type : String,
-            required :true
+            required :[true,'Password is required'],
+        },
+        avatar : {
+            type : String, // cloudnary services we use
+            required : true,
+            // default : "https://res.cloudinary.com/dzj8q4l9c/image/upload/v1700000000/default-avatar.png"
+        },
+        coverImage : {
+            type : String, // cloudnary services we use 
+            required : true,
+            // default : "https://res.cloudinary.com/dzj8q4l9c/image/upload/v1700000000/default-cover.jpg"
+        },
+        watchHistroy : [
+            {
+                type : mongoose.Schema.Types.ObjectId, 
+                ref : "Video",
+            }
+        ],
+        refreshToken:{
+            type : String,
         }
     },
     {timestamps:true}
